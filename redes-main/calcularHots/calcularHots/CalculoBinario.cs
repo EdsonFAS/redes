0b48d3791ecd7ac639a75d3f99f077ab71246e19
EdsonFAS <111073194+EdsonFAS@users.noreply.github.com>
2023-09-06 17:25:24 -0400

    Add files via upload

---
+++ redes-main/calcularHots/calcularHots/CalculoBinario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcularHots
+{
+    internal class CalculoBinario
+    {
+        public static int _ContUm { get; set; }
+        public static int _ContZero { get; set; }
+
+
+        public static void DecimalEmBinario(int numero)
+        {
+            _ContUm = 0;
+            _ContZero= 0;
+
+            string binario = "";
+            while (numero > 0)
+            {
+                int resto = numero % 2;
+                binario = resto.ToString() + binario;
+                numero /= 2;
+            }
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if (Convert.ToInt32(binario[i].ToString()) == 1)
+                {
+                    _ContUm++;
+                }
+                if (Convert.ToInt32(binario[i].ToString()) == 0)
+                {
+                    _ContZero++;
+                }
+            }
+            
+        }
+
+
+    }
+}
---
+++ redes-main/calcularHots/calcularHots/SalvarExc.cs
@@ -0,0 +1,43 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Information;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcularHots
+{
+    internal class SalvarExc
+    {
+        public static string[] coluna = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q" };
+        public static string[] linha = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+       
+
+        public static void salva(List<string> x, List<string> y)
+        {
+            string pasta = AppDomain.CurrentDomain.BaseDirectory;
+            pasta = pasta.Replace("\\bin\\Debug\\net6.0", "\\resp");
+            string nomeArquivo = $"arquivo_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+
+            string caminho = Path.Combine(pasta, nomeArquivo);
+            using (var package = new ExcelPackage())
+            {var worksheet = package.Workbook.Worksheets.Add("Planilha1");
+                for (int i = 0; i < x.Count; i++) { 
+                
+                // Adicione a string à célula A1 da planilha.
+                worksheet.Cells[$"{coluna[i]}{linha[0]}"].Value += x[i];
+                worksheet.Cells[$"{coluna[i]}{linha[1]}"].Value += y[i];}
+
+                // Salve o pacote Excel no arquivo especificado.
+
+                package.SaveAs(new System.IO.FileInfo(caminho));
+
+
+            }Process.Start("explorer.exe", pasta );
+        }
+
+    }
+}
